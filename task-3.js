0291560ddfef3e08d02ed4ed9b72f84a4ae72d63
Dmytrii Beresten <dmitryberesten@gmail.com>
2023-02-06 13:21:24 +0200

    домашнє завдання

---
+++ task-3.js
@@ -0,0 +1,31 @@
+// Завдання 3. Написати програму яка видає користувачу ввести число. Після введення числа вистрибує алерт з оголошенням пори року до якої відноситься число (номер місяця) що ввів користувач. Якщо число не входить в діапазон від 1 до 12 тоді неможливо.
+
+let month = parseInt(prompt("Напиши номер місяця"));
+
+if (month == 1) {
+    alert("Зима")
+} else if (month == 2) {
+    alert("Зима")
+} else if (month == 3) {
+    alert("Весна")
+} else if (month == 4) {
+    alert("Весна")
+} else if (month == 5) {
+    alert("Весна")
+} else if (month == 6) {
+    alert("Літо")
+} else if (month == 7) {
+    alert("Літо")
+} else if (month == 8) {
+    alert("Літо")
+} else if (month == 9) {
+    alert("Осінь")
+} else if (month == 10) {
+    alert("Осінь")
+} else if (month == 11) {
+    alert("Осінь")
+} else if (month == 12) {
+    alert("Зима")
+} else {
+    alert("Це не число місяця!")
+}
